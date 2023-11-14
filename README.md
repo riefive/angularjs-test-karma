737d9c99fe34a7a9f5dfd89e13b6cad2d5287506
riefive <rie.five@gmail.com>
2023-11-14 10:28:14 +0700

    chore: add multiple test

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ## References
 - https://www.guru99.com/angularjs-testing-unit.html
+- https://maksimrv.medium.com/unit-testing-javascript-with-karma-in-2021-starting-point-facc441b11c1
--- tests/foo.test.js
+++ tests/foo.test.js
@@ -1,5 +1,17 @@
+function multiply(x, y) {
+    return x * y;
+}
+
 describe('My first Karma test', function() {
     it('should be true', function() {
         expect(true).toEqual(true);
+    });
+
+    it('should be false', function() {
+        expect(false).toEqual(false);
+    });
+
+    it('should multiply 2 on 2', function() {
+        expect(multiply(2, 2)).toEqual(4);
     });
 });
